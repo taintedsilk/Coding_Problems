9756d32acc3cbe1de3acb1836767f377c8e04916
taintedsilk <congthanh4a2@gmail.com>
2021-10-13 11:30:36 +0700

    Update MODIFIED.cpp

--- MODIFIED.cpp
+++ MODIFIED.cpp
@@ -1,5 +1,18 @@
-// http://lequydon.ntucoder.net/Problem/Details/6366
+/* http://lequydon.ntucoder.net/Problem/Details/6366
+Ami có một dãy số A gồm n phần tử, được đánh số từ 1, phần tử thứ i của dãy số được kí hiệu là Ai. Ami có thể thực hiện thao tác sau với số lần bất kỳ : chọn một số Ai mà giá trị hiện tại lớn hơn 1 và trừ số này đi 1 đơn vị. Nếu số Ai đã bị trừ ít nhất một đơn vị, ta gọi số này đã bị biến đổi.
 
+Bây giờ, Ami cần tìm cách thực hiện thao tác sao cho số phần tử bị biến đổi là ít nhất, và tổng các phần từ sau khi thực hiện một số thao tác đúng bằng k.
+
+Input
+
+Dòng đầu tiên chứa 2 số nguyên dương n và k lần lượt là số phần tử, và tổng (n ≤ 105 , k ≤ 1016).
+
+Dòng tiếp theo chứa n số nguyên dương Ai là các phần tử dãy A (Ai ≤ 109).
+
+Output:
+
+Một số nguyên là số phần tử ít nhất bị biến đổi. Nếu không tồn tại bất kỳ cách thực hiện thao nào để tổng bằng k, in ra -1
+*/
 
 #include <iostream>
 #include <algorithm>
