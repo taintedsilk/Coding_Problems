eabd254023a528aa45fd190aa6759bb3af6f28c2
taintedsilk <92366795+taintedsilk@users.noreply.github.com>
2021-10-20 15:32:02 +0700

    Update bai13.cpp

--- BAITAP15-10/bai13.cpp
+++ BAITAP15-10/bai13.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <map>
 #include <stdio.h>
+#include <string>
 using namespace std;
 
 int main()
 {
-	freopen("INP.txt",  "r", stdin);
-	freopen("OUT.txt", "w", stdout);
+
     map<char, int>m;
     long long n;
     cin >> n;
     string a;
     cin >> a;
     m[a[0]] += 1;
-    long long prev_counter = 0;
+    string c = "";
     for (long long i = 1; i < n; i += 1) {
-        if (a[i] == 'B') {
-            if (prev_counter == 0) prev_counter += 1;
-            else prev_counter += 2;
-            m[a[i - prev_counter]] -= 1;}
+        if (a[i] == 'B') { // add && !c.empty() ?
+            m[a[c.length()]] -= 1;
+            c.pop_back(); //replace with c = substr(0, c.size - 1)
+
+        }
         else {
-        prev_counter = 0;
-        m[a[i]] += 1;
+            c += a[i];
+            m[a[i]] += 1;
+        }
     }
+    cout << m['G'] << " " << m['R'] - m['L'];
 }
-    cout << m['G'] << " " << m['R'] - m['L'];}
