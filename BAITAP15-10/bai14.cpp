5a4146e529a10a72aae5e36740878e01f92fddee
taintedsilk <92366795+taintedsilk@users.noreply.github.com>
2021-10-22 08:54:41 +0700

    Fixed

--- BAITAP15-10/bai14.cpp
+++ BAITAP15-10/bai14.cpp
@@ -3,15 +3,12 @@
 #include <cmath>
 #include <algorithm>
 #include <stdio.h>
-using namespace std;
-int main() {
-  #include <iostream>
-#include <math.h>
-#include <cmath>
-#include <algorithm>
 
 using namespace std;
-int main() {
+  freopen("INP.txt", "r", stdin);
+  freopen("OUT.txt", "w", stdout);
+int main() 
+{
 long long n;
 cin >> n;
 long long mid = n/2 + 1;
@@ -25,18 +22,5 @@ for (long long i = 0; i < a; i += 1) {
     ydis = abs(y - mid);
     s += min(xdis, ydis) * 15 + 10 * abs(xdis - ydis);
 }
-cout << s;}
-long long n;
-cin >> n;
-long long mid = n/2 + 1;
-long long a;
-cin >> a;
-long long x, y;
-long long xdis, ydis, s = 0;
-for (long long i = 0; i < a; i += 1) {
-    cin >> x >> y;
-    xdis = abs(x - mid);
-    ydis = abs(y - mid);
-    s += min(xdis, ydis) * 15 + 10 * abs(xdis - ydis);
+cout << s;
 }
-cout << s;}
