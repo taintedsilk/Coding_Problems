8b1b3d466ab94dbe658248b6be03dd160d5aed52
taintedsilk <92366795+taintedsilk@users.noreply.github.com>
2021-10-30 16:40:18 +0700

    Create Decor.cpp

---
+++ OnTap-30-10/Decor.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	freopen("decor.inp", "r", stdin);
+    freopen("decor.out", "w", stdout);
+    unsigned long long n;
+    cin >> n;
+    unsigned long long a = floor( ((double)sqrt(n)));
+    cout << fixed << a;}
