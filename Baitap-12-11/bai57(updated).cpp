7269006d6106355d3bf75a71cf36baceac513904
taintedsilk <92366795+taintedsilk@users.noreply.github.com>
2021-11-22 22:41:59 +0700

    Update and rename bai57(BIG FUCKING FUCKERY).cpp to bai57(updated).cpp

--- Baitap-12-11/bai57(updated).cpp
+++ Baitap-12-11/bai57(updated).cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-#include <sstream>
-#include <algorithm>
-#include <cmath>
-#include <math.h>
-#include <set>
-#include <stdio.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -16,22 +10,26 @@ string to_string(long long n) {
 
 string binary(long long n) {
     string bin = "";
+    stringstream ss;
+    string a;
     while (n != 0) {
-        bin += to_string(n % 2);
+        ss << n % 2;
+        ss >> a;
+        bin += a;
+        ss.clear();
         n /= 2;
     }
     return bin;}
 
 long long dec(string bin) {
-    reverse(bin.begin(), bin.end());
     long long number = 0;
     long long a;
     stringstream ss;
-    for (long long i = 0; i < bin.size(); i += 1) {
+    for (long long i = bin.size() - 1; i >= 0; i -= 1) {
         ss << bin[i];
         ss >> a;
         ss.clear();
-        number += a*(pow(2, i));
+        number += a*(pow(2, bin.size() - 1- i));
     }
     return number;}
 
@@ -40,18 +38,20 @@ int main()
 {
     freopen("INP.txt", "r", stdin);
     freopen("OUT.txt", "w", stdout);
-    set<long long>s;
+    unordered_set<long long>s;
     long long n;
     cin >> n;
     string bin = binary(n);
+    reverse(bin.begin(), bin.end());
     long long result = 0;
     for (long long i = 1; i <= bin.size(); i += 1) {
         for (long long j = i; j <= bin.size(); j += 1) {
             string a = bin.substr(j - i, i);
+            cout << a << " ";
             s.insert(dec(a));
         }
     }
-    set<long long>::iterator it;
+    unordered_set<long long>::iterator it;
     for (it = s.begin(); it != s.end(); it ++) {
         result += *it;
     }
