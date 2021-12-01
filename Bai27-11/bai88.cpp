7ce3fdfe83895cb3e5ce6ff80678431c0ff6c173
taintedsilk <92366795+taintedsilk@users.noreply.github.com>
2021-12-01 11:20:44 +0700

    Update bai88.cpp

--- Bai27-11/bai88.cpp
+++ Bai27-11/bai88.cpp
@@ -8,10 +8,14 @@ int main()
     freopen("OUT.txt", "w", stdout);
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    long long arr[8];
-    for (int i = 0; i < 8; i += 1) {
-        cin >> arr[i];
+    long long n, a;
+    cin >> n;
+    long long total = 0;
+    long long highest = LLONG_MIN;
+    for (int i = 0; i < n; i += 1) {
+        cin >> a;
+        total += a;
+        highest = max(a, highest);
     }
-    sort(arr, arr + 8);
-    cout << arr[7] + arr[6] + arr[5];
+    cout << max(highest * 2, total);
 }
