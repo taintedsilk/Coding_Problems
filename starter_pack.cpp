0af913544620ad1881cc828eea127c5c5380f43b
taintedsilk <92366795+taintedsilk@users.noreply.github.com>
2021-12-05 14:34:40 +0700

    Update starter_pack.cpp

--- starter_pack.cpp
+++ starter_pack.cpp
@@ -6,6 +6,18 @@ typedef long long ll;
 typedef bool bol;
 typedef string str;
 
-int main() {
-  
+int main()
+{
+    freopen("INP.txt", "r" ,stdin);
+    freopen("OUT.txt", "w", stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    //long long t;
+    //cin >> t;
+    for (int i = 0; i < 1; i += 1)
+    {
+        solve();
+        cout << "\n";
+    }
+    return 0;
 }
