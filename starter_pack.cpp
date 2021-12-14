69bb7fe58f9262692c82b3d738618c2dc8bce201
taintedsilk <92366795+taintedsilk@users.noreply.github.com>
2021-12-14 15:17:20 +0700

    Update starter_pack.cpp

--- starter_pack.cpp
+++ starter_pack.cpp
@@ -7,6 +7,22 @@ typedef bool bol;
 typedef string str;
 typedef unsigned long long ull;
 
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+unordered_map<ll, ll, custom_hash>m;
+
 void solve() {
 }
 int main()
